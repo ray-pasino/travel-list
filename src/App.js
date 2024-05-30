cc6860e38c81dc7b1b3e75e065d8ed681248a40f
ray-pasino <115047105+ray-pasino@users.noreply.github.com>
2024-05-30 07:19:06 +0000

    added interactivity to the form submission

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 
 const initialItems = [
   { id: 1, description: "Passports", quantity: 2, packed: false },
@@ -23,19 +23,43 @@ function Logo() {
 }
 
 function Form() {
+
+  const [description, setDescription] = useState("")
+  const [quantity, setQuantity] = useState(1)
+
+function handleSubmit (e){
+  e.preventDefault()
+
+  if(!description) return
+
+  const newItem = {description, quantity, packed:false, id: Date.now()}
+  console.log(newItem)
+
+  setDescription('')
+  setQuantity(1)
+}
+
   return (
-    <div className="add-form">
+    <form className="add-form" onSubmit={handleSubmit}>
       <h3>What do you need for your 😍 trip?</h3>
-    </div>
+      <select  onChange={(e) => setQuantity(Number(e.target.value))}>
+        {Array.from({length:20}, (_, i) => i+1).map(num => <option value={num} key={num}>
+          {num}
+        </option>)}
+      </select>
+      <input type="text" placeholder="Item..." value={description}
+      onChange={(e) => setDescription(e.target.value)}/>
+      <button>Add</button>
+    </form>
   );
 }
 
-function PackingList() {
+function PackingList() { 
   return (
     <div className="list">
       <ul>
         {initialItems.map((item) => (
-          <Item item={item} />
+          <Item item={item} key={item.id}/>
         ))}
       </ul>
     </div>
